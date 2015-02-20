3c3331af5e0c03ed62deb95b8483ed3ce392e327
Konnor95 <dima95kh@gmail.com>
2015-02-21 01:24:20 +0200

    Deploy

--- src/main/java/com/mprojection/controller/UserController.java
+++ src/main/java/com/mprojection/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user/")
+@RequestMapping("/api/user/")
 public class UserController {
 
     @Autowired
