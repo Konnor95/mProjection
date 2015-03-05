a56b30506b9f524c8dddc908e36e9340d94e5364
Konnor95 <dima95kh@gmail.com>
2015-03-05 08:53:50 +0200

    Deploy

--- src/main/java/com/mprojection/util/ArrayUtil.java
+++ src/main/java/com/mprojection/util/ArrayUtil.java
@@ -4,6 +4,13 @@ import java.util.Arrays;
 
 public final class ArrayUtil {
 
+    /**
+     * Merges two arrays.
+     * @param first first array
+     * @param second second array
+     * @param <T> array type
+     * @return merged values as array
+     */
     public static <T> T[] merge(T[] first, T[] second) {
         T[] result = Arrays.copyOf(first, first.length + second.length);
         System.arraycopy(second, 0, result, first.length, second.length);
